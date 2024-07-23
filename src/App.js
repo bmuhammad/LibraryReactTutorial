7ff19a7c5806f79cd9e865618936f516c077c21a
Brian <briancmuhammad@yahoo.com>
2024-07-22 20:05:54 -0400

    added featured books section with react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import Nav from './components/Nav'
+import Featured from './components/Featured';
+import Highlights from './components/Highlights';
+import Landing from './components/Landing';
+import Nav from './components/Nav';
+
 
 
 function App() {
   return (
     <div className="App">
 <Nav />
+<Landing />
+<Highlights />
+<Featured />
     </div>
   );
 }
