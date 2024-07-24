0d107ee889054789850bf7158af247c3f39c44d2
Brian <briancmuhammad@yahoo.com>
2024-07-23 20:17:30 -0400

    add all details of book to book page with recommened book

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 import Logo from "../assets/Library.svg";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer>
       <div className="container">
         <div className="row row__column">
-          <a href="/">
+          <Link to="/">
             <figure className="footer__logo">
               <img src={Logo} className="footer__logo--img" alt="" />
             </figure>
-          </a>
+          </Link>
           <div className="footer__list">
             <a href="/" className="footer__link">
               Home
             </a>
             <span className="footer__link no-cursor">About</span>
-            <a href="/books" className="footer__link">
+            <Link to="/books" className="footer__link">
               Books
-            </a>
+            </Link>
             <a href="/cart" className="footer__link">
               Cart
             </a>
